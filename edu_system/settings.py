7a4f838ca66f4362c63633ff838c595f41ed77ee
mo <504643635@qq.com>
2023-09-04 16:52:07 +0800

    添加读取json文件来连接mysql

--- edu_system/settings.py
+++ edu_system/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-7(&2@zfuky+c16lia($%#l)*(+9j!50=dcnyy*oxe8mhsb4&_=
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -80,17 +79,22 @@ APPEND_SLASH = False
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+MYSQL_FILE = os.path.join(BASE_DIR, 'mysql.json')
+with open(MYSQL_FILE, 'r', encoding="utf-8") as f:
+    tmp = json.load(f)
+USER = tmp.get('USER') if tmp.get('USER') else 'mo'
+PASSWORD = tmp.get('PASSWORD') if tmp.get('PASSWORD') else '1998'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'app_system',
-        'USER': 'root',
-        'PASSWORD': 'modengyi',
+        'USER': USER,
+        'PASSWORD': PASSWORD,
         'HOST': 'localhost',
         'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -110,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -121,7 +124,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
