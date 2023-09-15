6a239b8efee0a52bc6b3add82704f1576736c42d
mo <504643635@qq.com>
2023-09-15 10:46:27 +0800

    创建wechatapp

---
+++ wechat_req/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WechatReqConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wechat_req'
---
+++ wechat_req/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+"""
+首页
+"""
+def get_notifications():
+    """
+    获取通知列表
+    """
+    pass
+
+"""
+搜索页面
+"""
+"""
+我页面
+"""
+"""
+资源列表页
+"""
+"""
+音频资源详情页
+"""
+"""
+视频资源详情页
+"""
+"""
+文本资源详情页
+"""
