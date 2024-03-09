0875444e838a2762d1c2b81c4935a4aadd32fef3
DonaghyMO <504643635@qq.com>
2024-03-09 09:24:18 +0800

    category_id

--- wechat_req/views.py
+++ wechat_req/views.py
@@ -433,11 +433,7 @@ def wc_get_category(request):
         for pre, fill, node in RenderTree(root):
             full_nodes.append(int(node.name))
         cats = Category.objects.get(id=category_id)
-
-        print(cats.child_category)
-        print(cats.child_category.split(","))
-        print(len(cats.child_category.split(",")))
-        child_category = [int(i) for i in cats.child_category.split(",")]
+        child_category = [int(i) for i in cats.child_category.split(",") if i!='']
         children = Category.objects.filter(id__in=child_category)
         child_category = [{"id":i.id,"category_name":i.name} for i in children if i.id in full_nodes]
 
